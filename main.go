80e8ca2119f2378f1654e8a83fd86c1568ab33c3
EricZhou <dejavuzhou@qq.com>
2017-05-09 18:06:28 +0800

    新的开始

--- main.go
+++ main.go
@@ -1,16 +1,31 @@
 package main
 
 import (
+	"fmt"
 	_ "my_go_web/models"
 	_ "my_go_web/routers" //init reuters
 	_ "my_go_web/tools"   //init(register) html-template function ...
-	_ "my_go_web/tools/spiders"
 
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql" // import your used driver
 )
 
 func init() {
-
+	//config mysql database with config/app.config file
+	user := beego.AppConfig.String("mysqluser")
+	passwd := beego.AppConfig.String("mysqlpass")
+	host := beego.AppConfig.String("mysqlurls")
+	port, err := beego.AppConfig.Int("mysqlport")
+	dbname := beego.AppConfig.String("mysqldb")
+	if nil != err {
+		port = 3306
+	}
+	if beego.AppConfig.String("runmode") == "dev" {
+		orm.Debug = true
+	}
+	orm.RegisterDriver("mysql", orm.DRMySQL)
+	orm.RegisterDataBase("default", "mysql", fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8", user, passwd, host, port, dbname))
 }
 
 func main() {
--- tools/spiders/task.go
+++
@@ -1,3 +0,0 @@
-package spiders
-//https://github.com/RichardKnop/machinery
-//定时任务
