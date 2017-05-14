a2b626b9add80746480111167c9c1046d080a6b6
EricZhou <dejavuzhou@qq.com>
2017-05-14 13:32:18 +0800

    修复

--- static/js/my.js
+++ static/js/my.js
@@ -62,6 +62,41 @@ if (pageIdentifier == "article-index-page") {
                 domLoad: '<div class="dropload-load"><span class="loading"></span>加载中-了解更多美剧资讯学习更多潮流英语...</div>',
                 domNoData: '<div class="dropload-noData">暂无数据-了解更多美剧资讯学习更多潮流英语</div>'
             },
+            loadUpFn: function (me) {
+                let lastTime = $('section.content > div.well:last > div.media-body > ul > li:nth-child(2) > strong > span').data('time')
+                let param = { _xsrf: token, offset: tatalCount }
+                $.ajax({
+                    type: 'POST',
+                    url: 'article/load-more-news',
+                    dataType: param,
+                    success: function (data) {
+                        var result = '';
+                        for (var i = 0; i < data.lists.length; i++) {
+                            result += '<a class="item opacity" href="' + data.lists[i].link + '">'
+                                + '<img src="' + data.lists[i].pic + '" alt="">'
+                                + '<h3>' + data.lists[i].title + '</h3>'
+                                + '<span class="date">' + data.lists[i].date + '</span>'
+                                + '</a>';
+                        }
+                        // 为了测试，延迟1秒加载
+                        setTimeout(function () {
+                            $('.lists').html(result);
+                            // 每次数据加载完，必须重置
+                            me.resetload();
+                            // 重置页数，重新获取loadDownFn的数据
+                            page = 0;
+                            // 解锁loadDownFn里锁定的情况
+                            me.unlock();
+                            me.noData(false);
+                        }, 1000);
+                    },
+                    error: function (xhr, type) {
+                        alert('Ajax error!');
+                        // 即使加载出错，也得重置
+                        me.resetload();
+                    }
+                });
+            },
             loadDownFn: function (me) {
                 //last time 暂时不需要
                 let lastTime = $('section.content > div.well:last > div.media-body > ul > li:nth-child(2) > strong > span').data('time')
@@ -94,6 +129,9 @@ if (pageIdentifier == "article-index-page") {
                                 //获取excerpt
                                 let excerpt = stripHtml(item.Body)
 
+                                let date = item.CreatedAt.substring(0,10)
+                                let time = item.CreatedAt.substring(11,16)
+
                                 //html 输出
                                 result += '<div class="well-xs well media"><div class="media-left"><a href="/article/' 
                                                 + item.ID + '"><img src="https://oeveb4zm9.qnssl.com/'
@@ -102,8 +140,8 @@ if (pageIdentifier == "article-index-page") {
                                                 + item.ID+'"><h2 class="media-heading text-red text-center lead">'
                                                 + item.Title + '</h2></a><p class="text-excerpt">'
                                                 + excerpt + '</p><ul class="list-inline text-green"><li><strong><span class="fa fa-calendar" title="发布日期"></span></strong>'
-                                                + item.CreatedAt + '</li><li><strong><span class="ion-clock" title="发布时间 publiced time" data-time="2017-05-12 07:21:55"></span></strong>'
-                                                + item.CreatedAt + '</li><li><strong><span class="glyphicon glyphicon-eye-open" title="阅读数量 read count"></span></strong>'
+                                                + date + '</li><li><strong><span class="ion-clock" title="发布时间 publiced time" data-time="2017-05-12 07:21:55"></span></strong>'
+                                                + time + '</li><li><strong><span class="glyphicon glyphicon-eye-open" title="阅读数量 read count"></span></strong>'
                                                 + 1999 + '</li><li><strong><span class="ion-heart" title="收藏数量 favarate count"></span></strong>'
                                                 + 3123 + '</li><li><strong><span class="fa fa-commenting" title="评论数量 comment count"></span></strong>'
                                                 + 3434 + '</li></ul></div></div>';
@@ -116,7 +154,7 @@ if (pageIdentifier == "article-index-page") {
                             me.noData();
                         }
                         //插入dom
-                        $('section.content > div.well:last').append(result);
+                        $('section.content > div.well:last').after(result);
                         // 每次数据插入，必须重置
                         me.resetload();
                     },
