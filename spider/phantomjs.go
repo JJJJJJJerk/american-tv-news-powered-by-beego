a509311ff43b75aeb6fc70fb757cd05274e145bb
EricZhou <dejavuzhou@qq.com>
2017-05-25 21:57:00 +0800

    爬去电影天堂

--- spider/phantomjs.go
+++ spider/phantomjs.go
@@ -15,12 +15,10 @@ func init() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	if err := phantomjs.DefaultProcess.Open(); err != nil {
 		log.Fatal(err)
 	}
 	defer phantomjs.DefaultProcess.Close()
-
 	//穿件webpage
 	page, err := phantomjs.CreateWebPage()
 	if err != nil {
@@ -31,7 +29,7 @@ func init() {
 	webPageSettings := phantomjs.WebPageSettings{
 		JavascriptEnabled:             true,
 		LoadImages:                    false,
-		LocalToRemoteURLAccessEnabled: true,
+		LocalToRemoteURLAccessEnabled: true, //local script can asscess the remote files
 		UserAgent:                     "Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1",
 		Username:                      "dejavuzhou@qq.com",
 		Password:                      "ZHou1987",
@@ -47,7 +45,7 @@ func init() {
 	if err := page.SetLibraryPath(jqueryFileDir); err != nil {
 		log.Fatal(err)
 	}
-	if err := page.Open("http://www.163.com/"); err != nil {
+	if err := page.Open("http://www.dy2018.com/html/tv/oumeitv/index.html"); err != nil {
 		log.Fatal(err)
 	}
 	//注入js 文件必须在打开文件之后
@@ -55,21 +53,19 @@ func init() {
 		log.Fatal(err)
 	}
 
-	// Setup the viewport and render the results view.
-	if err := page.SetViewportSize(640, 960); err != nil {
-		log.Fatal(err)
-	}
-	if err := page.Render("haitou.png", "png", 100); err != nil {
-		log.Fatal(err)
-	}
 	// Read first link.
-	info, err := page.Evaluate(`function() {
-		var link = $('head > meta:nth-child(6)').attr('content');
-		return link;
+	array, err := page.Evaluate(`function() {
+		var array = new Array();
+		$('ul > table > tbody > tr:nth-child(2) > td:nth-child(2) > b > a').each(function(index,item){
+			var name = $(item).attr('title');
+			var href = $(item).attr('href');
+			array[index] = {name:name,href:href}
+		});
+		return array;
 	}`)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("json", info)
+	fmt.Println("json", array)
 
 }
