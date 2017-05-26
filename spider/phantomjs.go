f2a7e3a0c0f1a2a9eec1a282b76b7b300ae811d8
EricZhou <dejavuzhou@qq.com>
2017-05-26 23:19:16 +0800

    解决电影天堂二级页面美剧爬去

--- spider/phantomjs.go
+++ spider/phantomjs.go
@@ -9,6 +9,11 @@ import (
 	"github.com/benbjohnson/phantomjs"
 )
 
+type DygodItem struct {
+	Name string `json:",string"`
+	Href string `json:",string"`
+}
+
 func init() {
 	//获取软件的根目录
 	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
@@ -56,16 +61,89 @@ func init() {
 	// Read first link.
 	array, err := page.Evaluate(`function() {
 		var array = new Array();
+		var host = location.host;
 		$('ul > table > tbody > tr:nth-child(2) > td:nth-child(2) > b > a').each(function(index,item){
 			var name = $(item).attr('title');
-			var href = $(item).attr('href');
-			array[index] = {name:name,href:href}
+			var href = 'http://'+host + $(item).attr('href');
+			array.push({name:name,href:href})
 		});
 		return array;
 	}`)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("json", array)
+	items := array.([]interface{})
+
+	for _, item := range items {
+		node, ok := item.(map[string]interface{})
+		// e is the
+		name := node["name"].(string)
+		href := node["href"].(string)
+		fmt.Println(name, href, ok)
+
+		if err := page.Open(href); err != nil {
+			log.Fatal(err)
+		}
+
+		//注入js 文件必须在打开文件之后
+		if err := page.InjectJS("jquery.min.js"); err != nil {
+			log.Fatal(err)
+		}
+		//解析页面
+		jsonUU, err := page.Evaluate(`function() {
+		var h1 = $('h1').text();
+		var body =$('#Zoom').text();
+		return {title:h1,content:body};
+	}`)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		dic, ok := jsonUU.(map[string]interface{})
+		title := dic["title"].(string)
+		content := dic["content"].(string)
+
+		//储存数据结果
+		fmt.Println(content, title)
+	}
+
+	// var array = new Array();
+	// var array = $('#header > div > div.bd2 > div.bd3 > div.bd3r > div.co_area2 > div.co_content8 > ul > table > tbody > tr:nth-child(2) > td:nth-child(2) > b > a:nth-child(2)').each(function(index,value){var href = $(value).attr('href');var name=$(value).attr('title');array.push({href:href,name:name})});
+}
+
+func fetchDyGodPage(name, href string, page *phantomjs.WebPage) map[string]string {
+	if err := page.Open(href); err != nil {
+		log.Fatal(err)
+	}
+
+	//注入js 文件必须在打开文件之后
+	if err := page.InjectJS("jquery.min.js"); err != nil {
+		log.Fatal(err)
+	}
+	//解析页面
+	jsonUU, err := page.Evaluate(`function() {
+		var h1 = $('h1').text();
+		var body =$('#Zoom').text();
+		return {title:h1};
+	}`)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dic := jsonUU.(map[string]interface{})
+
+	//储存数据结果
+	fmt.Println(dic)
+
+	info := map[string]string{
+		"name": name,
+		// "title":       dic["title"].(string),
+		// "raw_content": dic["content"].(string),
+		"raw_href": href,
+	}
+	fmt.Println("\n\n\n\n", info)
+	return info
 
 }
