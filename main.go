a43ebf6b22cebb510bc30c9127b3ba3859c6f36c
EricZhou <dejavuzhou@qq.com>
2017-06-02 12:23:23 +0800

    定时任务

--- main.go
+++ main.go
@@ -9,10 +9,16 @@ import (
 	_ "my_go_web/tools" //init(register) html-template function ...
 
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/toolbox"
 )
 
 func main() {
-	spider.RunDygodMeijuSpider()
+	//spider.RunDygodMeijuSpider()
+	//没56分钟27秒执行一次;
+	//2两小时一次
+	taskSpiderDygodMeiju := toolbox.NewTask("taskSpiderDygodMeiju", "27 * */2 * * *", func() error { spider.RunDygodMeijuSpider(); return nil })
+	toolbox.AddTask("taskSpiderDygodMeiju", taskSpiderDygodMeiju)
+	toolbox.StartTask()
 
 	beego.Run()
 
