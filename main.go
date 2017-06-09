806463c51c38beef9f644646db8fe51ee88ec8b5
EricZhou <dejavuzhou@qq.com>
2017-06-10 02:23:26 +0800

    fix

--- main.go
+++ main.go
@@ -5,11 +5,9 @@ import (
 
 	_ "my_go_web/models"
 	_ "my_go_web/routers" //init reuters
-	"my_go_web/spider"
-	_ "my_go_web/tools" //init(register) html-template function ...
+	_ "my_go_web/tools"   //init(register) html-template function ...
 
 	"github.com/astaxie/beego"
-	"github.com/astaxie/beego/toolbox"
 )
 
 func main() {
@@ -17,8 +15,8 @@ func main() {
 	//没56分钟27秒执行一次;
 	//2两小时一次
 	//spider.RunDygodMeijuSpider()
-	taskSpiderDygodMeiju := toolbox.NewTask("taskSpiderDygodMeiju", "27 * */2 * * *", func() error { spider.RunDygodMeijuSpider(); return nil })
-	toolbox.AddTask("taskSpiderDygodMeiju", taskSpiderDygodMeiju)
+	//taskSpiderDygodMeiju := toolbox.NewTask("taskSpiderDygodMeiju", "27 * */2 * * *", func() error { spider.RunDygodMeijuSpider(); return nil })
+	//toolbox.AddTask("taskSpiderDygodMeiju", taskSpiderDygodMeiju)
 	//toolbox.StartTask()
 
 	beego.Run()
