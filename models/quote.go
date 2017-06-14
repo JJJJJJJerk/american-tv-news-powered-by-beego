31a256f3ce83185976c1d50d1bbf427d1ad70b1a
Eric <dejavuzhou@qq.com>
2017-06-14 22:55:23 +0800

    在golang里面怎么进行类型转换

--- models/quote.go
+++ models/quote.go
@@ -3,7 +3,9 @@ package models
 //http://jinzhu.me/gorm/ gorm 文档
 
 import (
+	"fmt"
 	"math/rand"
+	"strconv"
 	"time"
 
 	"github.com/jinzhu/gorm"
@@ -23,14 +25,19 @@ func Get3RandomQuote() (quotes []Quote) {
 	if CacheManager.IsExist(CK_QUOTE) {
 		value := CacheManager.Get(CK_QUOTE)
 		fmt.Println(value)
-		return CacheManager.Get(CK_QUOTE).([]Quote)
+		quotes = CacheManager.Get(CK_QUOTE).([]Quote)
 	} else {
-	var count int
-	Gorm.Model(&Quote{}).Count(&count)
-	rand.Seed(int64(count)) // Try changing this number!
-	indexs := []string{string(rand.Intn(count)), string(rand.Intn(count)), string(rand.Intn(count))}
-	Gorm.Model(&Quote{}).Where("id in (?)", indexs).Find(&quotes)
-	CacheManager.Put(CK_QUOTE, quotes, 600*time.Second)
+		var count int
+		Gorm.Model(&Quote{}).Count(&count)
+		rand.Seed(int64(count)) // Try changing this number!
+		na := strconv.Itoa(rand.Intn(count))
+		nb := strconv.Itoa(rand.Intn(count))
+		nc := strconv.Itoa(rand.Intn(count))
+		indexs := []string{na, nb, nc}
+		Gorm.Model(&Quote{}).Where("id in (?)", indexs).Find(&quotes)
+		CacheManager.Put(CK_QUOTE, quotes, 600*time.Second)
+
+	}
 	return
-	//}
+
 }
