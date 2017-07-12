832c4e74752f411de64aea3fc173acacbb7f8314
Eric <dejavuzhou@qq.com>
2017-07-12 22:40:14 +0800

    fix

--- routers/router.go
+++ routers/router.go
@@ -31,7 +31,4 @@ func init() {
 	// beego.AutoRouter(&controllers.ImageController{}) //对image使用自动路由
 	beego.Router("/subtitle", &controllers.SubtitleController{}, "get:Index")
 	beego.Router("/subtitle/load-more", &controllers.SubtitleController{}, "post:LoadMore")
-
-	beego.Router("/video/parse-url", &controllers.VideoController{}, "get:VideoUrlConvertToMp4Url")
-
 }
