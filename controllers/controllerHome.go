f7c0d652a9b27d64a357d4f393e3752fe93ffaef
Eric <dejavuzhou@qq.com>
2017-07-21 04:34:15 +0800

    添加logo

--- controllers/controllerHome.go
+++ controllers/controllerHome.go
@@ -13,10 +13,9 @@ func (c *HomeController) Get() {
 	models.Gorm.Limit(models.PageSize).Order("created_at DESC").Preload("Vote").Preload("Tags").Preload("Images").Find(&articles)
 	c.Data["BreadCrumbs"] = []Crumb{{"/", "fa fa-home", "首页"}, {"/article", "fa fa-fire", "资讯"}}
 	c.Data["Articles"] = articles
-	c.Data["Keyword"] = "提供最新最热的美剧资讯,美剧播放时间表,欧美搞笑有创意的短视频gif动图,英文电子有声读物,海量美剧双语原生字幕,这里是英语爱好者的乐园,让每一个人都爱上学习英语"
 	c.Data["Title"] = "娱乐学习英语"
 	c.Data["Keyword"] = "轻松学英语,欧美,搞笑gif动图短片,"
-	c.Data["Description"] = ""
+	c.Data["Description"] = "提供最新最热最快最热的美剧资讯,欧美搞笑有创意的短视频gif动图,海量美剧双语原生字幕,这里是英语爱好者的乐园,让每一个人都爱上学习英语"
 	c.Layout = "layout/base_index.html"
 	c.TplName = "home/index.html"
 }
