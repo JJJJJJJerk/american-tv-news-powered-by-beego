04457f868a2969f5eeb382bcbc077118dc275bf0
Eric <dejavuzhou@qq.com>
2017-07-21 04:35:38 +0800

    fix

--- controllers/controllerHome.go
+++ controllers/controllerHome.go
@@ -13,7 +13,7 @@ func (c *HomeController) Get() {
 	models.Gorm.Limit(models.PageSize).Order("created_at DESC").Preload("Vote").Preload("Tags").Preload("Images").Find(&articles)
 	c.Data["BreadCrumbs"] = []Crumb{{"/", "fa fa-home", "首页"}, {"/article", "fa fa-fire", "资讯"}}
 	c.Data["Articles"] = articles
-	c.Data["Title"] = "娱乐学习英语"
+	c.Data["Title"] = "mojoTV最新最快最热的美剧周边资讯"
 	c.Data["Keyword"] = "轻松学英语,欧美,搞笑gif动图短片,"
 	c.Data["Description"] = "提供最新最热最快最热的美剧资讯,欧美搞笑有创意的短视频gif动图,海量美剧双语原生字幕,这里是英语爱好者的乐园,让每一个人都爱上学习英语"
 	c.Layout = "layout/base_index.html"
