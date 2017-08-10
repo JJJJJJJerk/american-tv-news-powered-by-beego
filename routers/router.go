440731e8ef3ff33f714eaa7567bb7139fe5d63d5
Eric <dejavuzhou@qq.com>
2017-08-10 17:29:01 +0800

    dasf

--- routers/router.go
+++ routers/router.go
@@ -39,7 +39,7 @@ func init() {
 
 	beego.Router("/rrv/index", &controllers.RenRenController{}, "get:Index")
 	beego.Router("/rrv/m3u8/:episodeSid([0-9]+)", &controllers.RenRenController{}, "get:M3u8")
-	beego.Router("/rrv/search/:keyword:string", &controllers.RenRenController{}, "get:Search")
+	beego.Router("/rrv/search/:keyword", &controllers.RenRenController{}, "get:Search")
 	beego.Router("/rrv/top", &controllers.RenRenController{}, "get:Top")
 	beego.Router("/rrv/hot", &controllers.RenRenController{}, "get:Hot")
 	beego.Router("/rrv/season/:seasonId([0-9]+)", &controllers.RenRenController{}, "get:Season")
